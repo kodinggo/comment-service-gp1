11bb5874087f262a08e0f9c1def54f7207f22dc2
Reza Indra <edu.rezaindra@gmail.com>
2024-07-22 11:16:27 +0700

    add comment grpc

---
+++ internal/delivery/grpcsvc/comment_impl.go
@@ -0,0 +1,35 @@
+package grpcsvc
+
+import (
+	pb "comment-service-gp1/pb/comment"
+	"context"
+)
+
+type CommentService struct {
+	pb.UnimplementedCommentServiceServer
+}
+
+func NewCommentService() *CommentService {
+	return &CommentService{}
+}
+
+func (s *CommentService) FindAllCommentsByStoryID(context.Context, *pb.FindAllCommentsByStoryIDRequest) (*pb.Comments, error) {
+	comments := []*pb.Comment{
+		{
+			Id:      1,
+			Comment: "Ini contoh komentar",
+		},
+		{
+			Id:      2,
+			Comment: "hehehe",
+		},
+		{
+			Id:      2,
+			Comment: "Hanya coba saja",
+		},
+	}
+
+	return &pb.Comments{
+		Comments: comments,
+	}, nil
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"comment-service-gp1/internal/delivery/grpcsvc"
+	pb "comment-service-gp1/pb/comment"
+
+	"google.golang.org/grpc"
+)
+
+const grpcPort = 8083
+
+func main() {
+	// Run grpc server
+	grpcServer := grpc.NewServer()
+
+	commentService := grpcsvc.NewCommentService()
+
+	pb.RegisterCommentServiceServer(grpcServer, commentService)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Panicf("failed create http listener, error: %v", err)
+	}
+
+	log.Printf("grpc server is running with port: :%d", grpcPort)
+
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Panicf("failed run grpc server, error: %v", err)
+	}
+}
